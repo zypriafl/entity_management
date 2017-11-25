0556bb115bb89e671b06d88ead959030cfbbdaf8
Florian Zyprian <zypriafl@web.de>
2017-11-25 19:15:37 +0100

    fix(static): adapt static path to uberspace

--- config/production.py
+++ config/production.py
@@ -8,5 +8,5 @@ SECRET_KEY = os.environ['SECRET_KEY']
 RECAPTCHA_PUBLIC_KEY = '6LccWDoUAAAAAMv5iCOIzTPUYtf3i4ORKzWLXm7h'
 RECAPTCHA_PRIVATE_KEY = os.environ['RECAPTCHA_PRIVATE_KEY']
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+# Uberspace static dir
+STATIC_ROOT = '/home/study/html/static/'
