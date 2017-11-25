fa515a57fa83665acbff69c3aa092da72111d59f
Florian Zyprian <zypriafl@web.de>
2017-11-25 19:09:39 +0100

    fix(static): add path for static files

--- config/production.py
+++ config/production.py
@@ -7,3 +7,6 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 RECAPTCHA_PUBLIC_KEY = '6LccWDoUAAAAAMv5iCOIzTPUYtf3i4ORKzWLXm7h'
 RECAPTCHA_PRIVATE_KEY = os.environ['RECAPTCHA_PRIVATE_KEY']
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
