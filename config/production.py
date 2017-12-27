9955de4520811d93c45bba7e6f4decfc68042402
Florian Zyprian <zypriafl@web.de>
2017-12-27 15:52:53 +0100

    chore(config): remove old example password

--- config/production.py
+++ config/production.py
@@ -15,9 +15,9 @@ RECAPTCHA_PRIVATE_KEY = os.environ['RECAPTCHA_PRIVATE_KEY']
 
 # Email Settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.environ['EMAIL_HOST']  # 'smtp.mailgun.org'
-EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']  # 'noreply@mg.studylife-muenchen.de'
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']  # '7QwslqAOjvoQmYxO7S'
+EMAIL_HOST = os.environ['EMAIL_HOST']
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 ADMINS = [admin for admin in os.environ['ADMINS'].split(', ')]
